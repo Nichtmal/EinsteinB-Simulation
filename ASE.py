Added normalization of unperturbed WF and smaller fixes

ASE.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ase import Atoms
 from gpaw import GPAW
 from gpaw.utilities.ps2ae import PS2AE
@@ -35,6 +36,7 @@ def run_simulation(
     """
     atom_label = f"{element}+" if ionized else element
     print(atom_label)
+    dx = grid_spacing * 1e-10  # Integration element in SI units
 
     # Define the atom
     adjusted_dimension = (
@@ -83,9 +85,10 @@ def run_simulation(
 
     for n in range(num_states):
         for s in [0, 1]:
+            wf = wfs.get_wave_function(k=0, n=n, s=s)
             state_data = {
                 "eigenvalue": eigenvalues[n],
-                "wavefunction": wfs.get_wave_function(k=0, n=n, s=s),
+                "wavefunction": wf / np.sqrt(np.sum(np.conj(wf) * wf) * dx**3),
                 "number": n,
                 "spin": s,
                 "atom": atom_label,

Einstein.py
@@ -1,6 +1,6 @@
 from scipy.special import wofz
 import numpy as np
-from scipy.constants import hbar, alpha, c, k, e, m_e
+from scipy.constants import hbar, alpha, c, e, m_e
 
 from Perturbation import generate_radial_grid
 
@@ -8,10 +8,6 @@ A = 1e-10  # 1 Angstrom in m
 gamma = e / (2 * m_e)
 
 Efactor = 4 * np.pi * alpha * c / (3 * hbar)
-
-# Setup Voigt function, both constant for f_0 ~ f(B)
-delta_nu_D = 1.7e9  # Doppler FWHM (Hz)
-delta_nu_L = 3e9  # Pressure FWHM (Hz)
 
 
 def integral_f(state1, state2, r_grid, dx):
@@ -64,7 +60,7 @@ def is_transition_allowed(state1, state2):
         return False
 
     if abs(mj1 - mj2) > 1:
-        print("Ms dont match")
+        # print("Ms dont match")
         return False
 
     if s1 != s2:
@@ -125,8 +121,16 @@ def g_j(j, l, s):
 
 
 def h_j(j, l, n):
+    """
+    Computes factor for SOC energy difference
+
+    """
+    if n == 0:
+        raise ValueError
     return (
         (j * (j + 1) - l * (l + 1) - 3 / 4) / (l * (l + 1) * (l + 1 / 2)) / (4 * n**3)
+        if l != 0  # SOC only takes effect with l > 0
+        else 0
     )
 
 
@@ -137,8 +141,8 @@ def delta_Ef(state_attr1, state_attr2, B, Z):
     s2 = j2 - l2
     g_j1 = g_j(j1, l1, s1)
     g_j2 = g_j(j2, l2, s2)
-    h_j1 = g_j(j1, l1, n1)
-    h_j2 = g_j(j2, l2, n1)
+    h_j1 = h_j(j1, l1, n1)
+    h_j2 = h_j(j2, l2, n1)
 
     factor_SOC = m_e * c**2 * alpha**4 * Z**4
 
@@ -181,6 +185,7 @@ def calculate_ESC(data, Z, desired_transitions):
     gridsize = dx
     x_max = float(0.5 * dx * points_n)
     print(f"X_max: {x_max}")
+    print(f"dx = {dx}")
 
     print(points_n)
     r_grid = generate_radial_grid(points_n, 2 * x_max)
@@ -237,7 +242,7 @@ def calculate_ESC(data, Z, desired_transitions):
                                 "B": B,
                             }
                             if R in results["states"]:
-                                print("Same same")
+                                print(f"E12 already in results \rState:{R}")
                             results["states"].append(R)
     print(f"Amount of Transitions: {a}")
     print("Einsteincoefficients calculated")

Perturbation.py
@@ -52,6 +52,10 @@ def m_ls(l):
     return [m - l for m in range(2 * l + 1)]
 
 
+def norm(psi, dx):
+    return np.sqrt(np.sum(np.conj(psi) * psi) * dx**3)
+
+
 # Defining Operators
 def S_x(s):
     return s * hbar * sigma_x
@@ -110,16 +114,16 @@ def Zeeman_perturbation(mls, s, grid):
     return factor * truncate_sparse_matrix(sparse.kron(sparse.eye(dim), L_S), grid)
 
 
-def perturbing(H, psi, eigenvalue, psi_pert, eigenvalue_pert):
+def perturbing(H, psi, eigenvalue, psi_pert, eigenvalue_pert, dx):
     """
     Perturb the given wave function psi with the wave function psi_pert using first-order perturbation theory.
 
     Args:
-        H (_type_): Perturbing Hamiltonian
-        psi (_type_): Original wave function (eigenstate)
-        eigenvalue (_type_): Eigenvalue for the original wave function
-        psi_pert (_type_): Perturbing wave function (eigenstate)
-        eigenvalue_pert (_type_): Eigenvalue for the perturbing wave function
+        H (np.array): Perturbing Hamiltonian
+        psi (np.array): Original wave function (eigenstate)
+        eigenvalue (float): Eigenvalue for the original wave function
+        psi_pert (np.array): Perturbing wave function (eigenstate)
+        eigenvalue_pert (float): Eigenvalue for the perturbing wave function
 
     Raises:
         ValueError: Is raised when eigenstate and matrix have incompatible dimensions
@@ -356,6 +360,9 @@ def correct_wavefunctions(data, length, desired_states, Z_eff, Bs):
         "states": [],
     }
 
+    for i in range(len(data["states"])):
+        data["states"][i]["wavefunction"] /= norm(data["states"][i]["wavefunction"], dx)
+
     for d_state in desired_states:
         n_d, l_d = d_state
         print(n_d, l_d)
@@ -407,7 +414,7 @@ def correct_wavefunctions(data, length, desired_states, Z_eff, Bs):
                             j = j_down
                             for B in Bs:
                                 H_down = H_SOC_down + B * H_Zee_down
-                                sum_down = 0
+                                sum = 0
                                 for state2 in data["states"]:
                                     n, l = get_qn(state2["number"])
                                     spin = state2["spin"]
@@ -418,7 +425,7 @@ def correct_wavefunctions(data, length, desired_states, Z_eff, Bs):
                                             n,
                                             l,
                                         ) != d_state:  # Preventing degenerate states from acting on each other
-                                            sum_down += perturbing(
+                                            sum += perturbing(
                                                 H_down,
                                                 psi,
                                                 eigenvalue,
@@ -429,13 +436,14 @@ def correct_wavefunctions(data, length, desired_states, Z_eff, Bs):
 
                                 # Debugging
                                 print(f"B = {B} T")
-                                # print(f"Hamiltonian: {B * H_Zee_down}")
-                                norm_corr = np.sum(np.abs(psi + sum_down) ** 2) * dx**3
-                                psi_corr = (psi + sum_down) / np.sqrt(
+                                norm_corr = norm(psi + sum, dx)
+                                norm_psi = norm(psi, dx)
+                                print(f"Norm psi = {norm_psi}")
+                                psi_corr = (psi + sum) / np.sqrt(
                                     norm_corr
                                 )  # Nomalize the resulting functions
                                 print(
-                                    f"Relative norm in order of {np.sqrt(np.sum(np.abs(sum_down) ** 2) * dx**3 / norm_corr)}"
+                                    f"Relative norm in order of {norm(sum, dx) / norm_psi}"
                                 )
 
                                 print(f"dx = {dx}")
@@ -446,7 +454,7 @@ def correct_wavefunctions(data, length, desired_states, Z_eff, Bs):
                             j = j_up
                             for B in Bs:
                                 H_up = H_SOC_up + B * H_Zee_up
-                                sum_up = 0
+                                sum = 0
                                 for state2 in data["states"]:
                                     n, l = get_qn(state2["number"])
                                     spin = state2["spin"]
@@ -457,7 +465,7 @@ def correct_wavefunctions(data, length, desired_states, Z_eff, Bs):
                                             n,
                                             l,
                                         ) != d_state:  # Preventing degenerate states from acting on each other
-                                            sum_up += perturbing(
+                                            sum += perturbing(
                                                 H_up,
                                                 psi,
                                                 eigenvalue,
@@ -466,10 +474,10 @@ def correct_wavefunctions(data, length, desired_states, Z_eff, Bs):
                                                 dx,
                                             )
                                 norm_corr = np.sqrt(
-                                    (np.sum(np.abs(psi + sum_up) ** 2) * dx**3)
+                                    (np.sum(np.abs(psi + sum) ** 2) * dx**3)
                                 )
 
-                                psi_corr = (psi + sum_up) / norm_corr
+                                psi_corr = (psi + sum) / norm_corr
                                 B_fields.append(B)
                                 Psis.append(psi_corr)
                         else:

database_manager.py
@@ -202,8 +202,8 @@ def corr_to_db(desired_states, Z_eff, Bs, set_name, Atom, database_file):
         # Initialize empty datasets for corrected states with proper dtypes
         corr_group.create_dataset("n", shape=(0,), maxshape=(None,), dtype="i4")
         corr_group.create_dataset("l", shape=(0,), maxshape=(None,), dtype="i4")
-        corr_group.create_dataset("j", shape=(0,), maxshape=(None,), dtype="i4")
-        corr_group.create_dataset("m_j", shape=(0,), maxshape=(None,), dtype="i4")
+        corr_group.create_dataset("j", shape=(0,), maxshape=(None,), dtype="f8")
+        corr_group.create_dataset("m_j", shape=(0,), maxshape=(None,), dtype="f8")
         corr_group.create_dataset(
             "Bs",
             shape=(0,),
@@ -214,7 +214,7 @@ def corr_to_db(desired_states, Z_eff, Bs, set_name, Atom, database_file):
             "wavefunctions",
             shape=(0, n, m),
             maxshape=(None, n, m),
-            dtype="complex128",
+            dtype="c16",
         )
         corr_group.create_dataset(
             "base_energy", shape=(0,), maxshape=(None,), dtype="float64"
@@ -235,7 +235,7 @@ def corr_to_db(desired_states, Z_eff, Bs, set_name, Atom, database_file):
 
             # Convert data to Numpy arrays for consistency
             bs_array = np.asarray(state["Bs"], dtype="float64")
-            wavefunctions_array = np.asarray(state["wavefunctions"], dtype="complex64")
+            wavefunctions_array = np.asarray(state["wavefunctions"], dtype="c16")
 
             # Write data into the datasets
             corr_group["n"][-1] = state["n"]